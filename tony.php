ca41fc2c49daebbcc7afb5f3279721a0fccc51a6
nmwamlima <mwamlimantonsite@gmail.com>
2021-06-18 10:38:32 +0300

    Script modified

--- tony.php
+++ tony.php
@@ -13,21 +13,47 @@ function split($message){
 	for($i=1; $i < $limit; $i++){
 		$break = explode(':',$message_parsed[$i]);
 		$start = array();
-
+		
 		$start[$break[0]] = $break[1];
+		$start['transactionReference'] = $break[1];
+		unset($start[$break[0]]);
+
 		$start[$break[2]] = $break[3];
+		$start['transactionType'] = $break[3];
+		unset($start[$break[2]]);
+
 		$start[$break[4]] = $break[5];
+		$start['transactionDetails'] = $break[5];
+		unset($start[$break[4]]);
+
 		$start[$break[6]] = $break[7];
+		$start['moreDetails'] = $break[7];
+		unset($start[$break[6]]);
+
 		$start[$break[8]] = $break[9];
+		$start['accountDetails'] = $break[9];
+		unset($start[$break[8]]);
+
 		$start[$break[10]] = $break[11];
+		$start['sender'] = $break[11];
+		unset($start[$break[10]]);
+
 		$start[$break[12]]= $break[13];
+		$start['nameBeneficiary'] = $break[13];
+		unset($start[$break[12]]);
+
 		$start[$break[14]]= $break[15];
+		$start['paymentDescription'] = $break[13];
+		unset($start[$break[12]]);
+
 		$start[$break[16]] = explode('}',$break[17])[0];
+		$start['fees'] = $start[$break[16]];
+		unset($start[$break[16]]);
+
 		array_push($info,$start);
 	}
 	foreach ($info as $data) {
 		print_r($data);
-		// mbona hii sio echo 😂😂😂
 	}
 }
 ?>
